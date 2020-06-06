0c311acafd4baceeae3c0551486911bac3ceadd3
Charles Wefso <cwefso@gmail.com>
2020-06-06 08:06:04 -0600

    Displays full iteration 2 travelers data

--- src/user.js
+++ src/user.js
@@ -3,6 +3,8 @@ class User {
     id !== undefined ? this.id = id : this.id = 1
     this.username = 'traveler' + id
     this.password = 'travel2020'
+    this.today = new Date();
+    this.date = this.today.getFullYear()+'-'+(this.today.getMonth()+1)+'-'+this.today.getDate();
   }
 
 }
